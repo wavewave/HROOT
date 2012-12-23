7001906c5a12b1e68817cc94e914468060fa57e6
Ian-Woo Kim <ianwookim@gmail.com>
2012-12-23 17:18:55 -0500

    separate MakePkg

--- lib/HROOT/Generate/MakePkg.hs
+++ lib/HROOT/Generate/MakePkg.hs
@@ -2,7 +2,7 @@
 
 -----------------------------------------------------------------------------
 -- |
--- Executable  : HROOT-generate
+-- Module      : HROOT.Generate.MakePkg
 -- Copyright   : (c) 2011, 2012 Ian-Woo Kim
 -- 
 -- License     : GPL-3
@@ -14,13 +14,12 @@
 --
 -----------------------------------------------------------------------------
 
-module Main where
+module HROOT.Generate.MakePkg where
 
-{-
-
-
-import           Data.Configurator as C
-import           Data.Configurator.Types 
+import           Control.Applicative
+import           Control.Monad
+-- import           Data.Configurator as C
+-- import           Data.Configurator.Types 
 import           Data.List 
 import qualified Data.Map as M
 import           Data.Maybe
@@ -29,7 +28,7 @@ import           Distribution.PackageDescription hiding (exposedModules)
 import           Distribution.PackageDescription.Parse
 import           Distribution.Verbosity
 import           Distribution.Version 
-
+import           System.Console.CmdArgs
 import           System.Directory
 import           System.FilePath 
 import           System.IO
@@ -39,54 +38,30 @@ import           Text.StringTemplate hiding (render)
 import           Bindings.Cxx.Generate.Code.Cabal
 import           Bindings.Cxx.Generate.Code.Cpp
 import           Bindings.Cxx.Generate.Code.Dependency
-
+import           Bindings.Cxx.Generate.Config
 import           Bindings.Cxx.Generate.Generator.ContentMaker 
 import           Bindings.Cxx.Generate.Generator.Driver
 import           Bindings.Cxx.Generate.Type.Class
 import           Bindings.Cxx.Generate.Util
 -- 
-import           Command
 -- import           HROOT.Generate.ROOT
 -- import           HROOT.Generate.ROOTAnnotate
 -- import           HROOT.Generate.ROOTModule
 import           HROOT.Generate.ROOTsmall
 import           HROOT.Generate.ROOTAnnotatesmall
-import           HROOT.Generate.ROOTModulesmall -}
-
-import           Control.Applicative
-import           Control.Monad
-import           Data.Configurator as C
-import           Data.Configurator.Types 
-import           System.Console.CmdArgs
--- 
-import           Bindings.Cxx.Generate.Config
--- 
-import           Command
-import           HROOT.Generate.MakePkg 
+import           HROOT.Generate.ROOTModulesmall
 -- 
 import qualified Paths_HROOT_generate as H
 import qualified Paths_fficxx as F
 
+{- 
 main :: IO () 
 main = do 
   param <- cmdArgs mode
   putStrLn $ show param 
   commandLineProcess param 
+-}
 
-commandLineProcess :: HROOTGenerate -> IO () 
-commandLineProcess (Generate conf) = do 
-  putStrLn "Automatic HROOT binding generation" 
-  cfg <- load [Required conf] 
-  mfficxxcfg <- liftM3 FFICXXConfig  
-                <$> C.lookup cfg "scriptbase" 
-                <*> C.lookup cfg "workingdir"
-                <*> C.lookup cfg "installbase"  
-  case mfficxxcfg of 
-    Nothing -> error "config file is not parsed well"
-    Just config -> makePackage config 
-
-
-{-
 cabalTemplate :: String 
 cabalTemplate = "Pkg.cabal"
 
@@ -152,65 +127,55 @@ getHROOTVersion conf = do
   let vnums = versionBranch . pkgVersion . package . packageDescription $ gdescs 
   return $ intercalate "." (map show vnums)
 
--- | 
-commandLineProcess :: HROOTGenerate -> IO () 
-commandLineProcess (Generate conf) = do 
-  putStrLn "Automatic HROOT binding generation" 
-  cfg <- load [Required conf] 
-  mfficxxcfg <- liftM3 FFICXXConfig  
-                <$> C.lookup cfg "scriptbase" 
-                <*> C.lookup cfg "workingdir"
-                <*> C.lookup cfg "installbase"  
-  case mfficxxcfg of 
-    Nothing -> error "config file is not parsed well"
-    Just config -> do 
-      let workingDir = fficxxconfig_workingDir config 
-          ibase = fficxxconfig_installBaseDir config
-          cabalFileName = pkgname <.> "cabal" -- cabalTemplate -- "HROOT.cabal"
-          (root_all_modules,root_all_classes_imports) = 
-            mkAllClassModulesAndCIH (pkgname,mkCROOTIncludeHeaders) root_all_classes
-      putStrLn "cabal file generation" 
-      getHROOTVersion config
-      copyPredefinedFiles ibase 
-      withFile (workingDir </> cabalFileName) WriteMode $ 
-        \h -> do 
-          mkCabalFile config h root_all_modules 
-      templateDir <- F.getDataDir >>= return . (</> "template")
-      (templates :: STGroup String) <- directoryGroup templateDir 
-      let cglobal = mkGlobal root_all_classes
-      -- 
-      putStrLn "header file generation"
-      writeTypeDeclHeaders templates cglobal workingDir pkgname root_all_classes_imports
-      mapM_ (writeDeclHeaders templates cglobal workingDir pkgname) root_all_classes_imports
-      -- 
-      putStrLn "cpp file generation" 
-      mapM_ (writeCppDef templates workingDir) root_all_classes_imports
-      -- 
-      putStrLn "RawType.hs file generation" 
-      mapM_ (writeRawTypeHs templates workingDir hprefix) root_all_modules 
-      -- 
-      putStrLn "FFI.hsc file generation"
-      mapM_ (writeFFIHsc templates workingDir hprefix) root_all_modules
-      -- 
-      putStrLn "Interface.hs file generation" 
-      mapM_ (writeInterfaceHs annotateMap templates workingDir hprefix) root_all_modules
-      -- 
-      putStrLn "Cast.hs file generation"
-      mapM_ (writeCastHs templates workingDir hprefix) root_all_modules
-      -- 
-      putStrLn "Implementation.hs file generation"
-      mapM_ (writeImplementationHs annotateMap templates workingDir hprefix) root_all_modules
-      -- 
-      putStrLn "module file generation" 
-      mapM_ (writeModuleHs templates workingDir hprefix) root_all_modules
-      -- 
-      putStrLn "HROOT.hs file generation"
-      writePkgHs (pkgname,hprefix) templates workingDir root_all_modules
-      -- 
-      copyFile (workingDir </> cabalFileName)  ( ibase </> cabalFileName ) 
-      copyPredefined templateDir (srcDir ibase) pkgname
-      mapM_ (copyCppFiles workingDir (csrcDir ibase) pkgname) root_all_classes_imports
-      mapM_ (copyModule workingDir (srcDir ibase) hprefix pkgname) root_all_modules 
+
+makePackage :: FFICXXConfig -> IO () 
+makePackage config = do 
+    let workingDir = fficxxconfig_workingDir config 
+        ibase = fficxxconfig_installBaseDir config
+        cabalFileName = pkgname <.> "cabal" -- cabalTemplate -- "HROOT.cabal"
+        (root_all_modules,root_all_classes_imports) = 
+          mkAllClassModulesAndCIH (pkgname,mkCROOTIncludeHeaders) root_all_classes
+    putStrLn "cabal file generation" 
+    getHROOTVersion config
+    copyPredefinedFiles ibase 
+    withFile (workingDir </> cabalFileName) WriteMode $ 
+      \h -> do 
+        mkCabalFile config h root_all_modules 
+    templateDir <- F.getDataDir >>= return . (</> "template")
+    (templates :: STGroup String) <- directoryGroup templateDir 
+    let cglobal = mkGlobal root_all_classes
+    -- 
+    putStrLn "header file generation"
+    writeTypeDeclHeaders templates cglobal workingDir pkgname root_all_classes_imports
+    mapM_ (writeDeclHeaders templates cglobal workingDir pkgname) root_all_classes_imports
+    -- 
+    putStrLn "cpp file generation" 
+    mapM_ (writeCppDef templates workingDir) root_all_classes_imports
+    -- 
+    putStrLn "RawType.hs file generation" 
+    mapM_ (writeRawTypeHs templates workingDir hprefix) root_all_modules 
+    -- 
+    putStrLn "FFI.hsc file generation"
+    mapM_ (writeFFIHsc templates workingDir hprefix) root_all_modules
+    -- 
+    putStrLn "Interface.hs file generation" 
+    mapM_ (writeInterfaceHs annotateMap templates workingDir hprefix) root_all_modules
+    -- 
+    putStrLn "Cast.hs file generation"
+    mapM_ (writeCastHs templates workingDir hprefix) root_all_modules
+    -- 
+    putStrLn "Implementation.hs file generation"
+    mapM_ (writeImplementationHs annotateMap templates workingDir hprefix) root_all_modules
+    -- 
+    putStrLn "module file generation" 
+    mapM_ (writeModuleHs templates workingDir hprefix) root_all_modules
+    -- 
+    putStrLn "HROOT.hs file generation"
+    writePkgHs (pkgname,hprefix) templates workingDir root_all_modules
+    -- 
+    copyFile (workingDir </> cabalFileName)  ( ibase </> cabalFileName ) 
+    copyPredefined templateDir (srcDir ibase) pkgname
+    mapM_ (copyCppFiles workingDir (csrcDir ibase) pkgname) root_all_classes_imports
+    mapM_ (copyModule workingDir (srcDir ibase) hprefix pkgname) root_all_modules 
 
 
--}
