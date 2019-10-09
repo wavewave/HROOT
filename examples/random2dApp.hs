83fc98a618d1fe584b36af6b1d18df2d7f11b922
Ian-Woo Kim <ianwookim@gmail.com>
2019-10-09 15:41:11 -0700

    modernize random2dApp

--- examples/random2dApp.hs
+++ examples/random2dApp.hs
@@ -1,36 +1,52 @@
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# OPTIONS_GHC -fno-warn-orphans #-}
+
 module Main where
 
-import Control.Monad
-
+-- import Control.Monad
+import Data.String           ( IsString(fromString) )
+import Foreign.C.Types       ( CDouble, CInt )
+import Foreign.C.String      ( CString, newCString )
+import Foreign.Marshal.Alloc ( alloca )
+import Foreign.Storable      ( poke )
+import System.IO.Unsafe      ( unsafePerformIO )
+--
 import HROOT
 
-main :: IO () 
-main = do 
+instance IsString CString where
+  fromString s = unsafePerformIO $ newCString s
 
-  tapp <- newTApplication "test" [0] ["test"]
-  tcanvas <- newTCanvas "Test" "Test" 640 480
-  h2 <- newTH2F "test" "test" 100 (-5.0) 5.0 100 (-5.0) 5.0 
-  tRandom <- newTRandom 65535
+main :: IO ()
+main = do
+  alloca $ \pargc -> do
+    alloca $ \pargv -> do
+      poke pargc (0::CInt)
+      poke pargv (""::CString)
+      tapp <- newTApplication ("test"::CString) pargc pargv
+      tcanvas <- newTCanvas ("Test"::CString) ("Test"::CString) 640 480
+      h2 <- newTH2F ("test"::CString) ("test"::CString) 100 (-5.0) 5.0 100 (-5.0) 5.0
+      tRandom <- newTRandom 65535
 
-  let dist1 = gaus tRandom 0 2 
-      dist2 = gaus tRandom 0 2 
+      let dist1 = gaus tRandom 0 2
+          dist2 = gaus tRandom 0 2
 
-  let go n | n < 0 = return () 
-           | otherwise = do 
-               histfill dist1 dist2 h2
-               go (n-1) 
+      let go n | n < 0 = return ()
+               | otherwise = do
+                   histfill dist1 dist2 h2
+                   go (n-1)
 
-  go 1000000
-  draw h2 "lego"
-  run tapp 1 
-  delete h2
-  delete tapp
+      go 1000000
+      draw h2 ("lego"::CString)
+      run tapp 1
+      delete h2
+      delete tapp
 
 
 
-histfill :: IO Double -> IO Double-> TH2F ->  IO () 
-histfill dist1 dist2 hist = do 
+histfill :: IO CDouble -> IO CDouble-> TH2F ->  IO ()
+histfill dist1 dist2 hist = do
   x <- dist1
   y <- dist2
-  fill2 hist x y 
-  return () 
+  fill2 hist x y
+  return ()
