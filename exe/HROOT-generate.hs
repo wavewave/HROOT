1cfa4618e9ab3c4eea7a00273d1c9a71cda9e114
Ian-Woo Kim <ianwookim@gmail.com>
2019-10-09 15:19:21 -0700

    remove dead code.

--- exe/HROOT-generate.hs
+++ exe/HROOT-generate.hs
@@ -119,7 +119,6 @@ pkgHsTemplate =
   \\n\
   \$topLevelDef\n"
 
-
 makeUmbrellaCabal :: String
 makeUmbrellaCabal =
   let pkgname = "HROOT"
@@ -207,8 +206,6 @@ makeUmbrellaPackage config mods = do
   putStrLn "copying"
   copyFileWithMD5Check (workingDir </> cabalFileName)  (installDir </> cabalFileName)
   copyFileWithMD5Check (workingDir </> pkg_summarymodule <.> "hs") (installDir </> "src" </> pkg_summarymodule <.> "hs")
-
-
 
 main :: IO ()
 main = do
@@ -310,7 +307,6 @@ main = do
                    , sbcStaticFiles = ["CHANGES","Config.hs","LICENSE","Setup.lhs"]
                    }
 
-
   simpleBuilder (mkcfg "HROOT-core")            sbc_core
   simpleBuilder (mkcfg "HROOT-graf")            sbc_graf
   simpleBuilder (mkcfg "HROOT-hist")            sbc_hist
@@ -320,71 +316,5 @@ main = do
   simpleBuilder (mkcfg "HROOT-RooFit-RooStats") sbc_roostats
   simpleBuilder (mkcfg "HROOT-tree")            sbc_tree
 
-
   -- RooFit and RooStats are not part of main HROOT.
   makeUmbrellaPackage (mkcfg "HROOT") [ "HROOT.Core", "HROOT.Hist", "HROOT.Graf", "HROOT.IO", "HROOT.Math", "HROOT.Tree" ]
-
-
-
-{-
-pkg_HROOT = PkgCfg { pkgname = "HROOT"
-                   , pkg_summarymodule = "HROOT"
-                   , pkg_typemacro = TypMcro ""
-                   , pkg_classes = []
-                   , pkg_cihs = []
-                   , pkg_modules = []
-                   , pkg_annotateMap = M.empty  -- for the time being
-                   , pkg_deps = [ "HROOT-core", "HROOT-hist", "HROOT-math"
-                                , "HROOT-tree", "HROOT-graf","HROOT-io"
-                                ]
-                   , pkg_hsbootlst = []
-                   , pkg_synopsis = "Haskell binding to the ROOT data analysis framework"
-                   , pkg_description = "HROOT is a haskell Foreign Function Interface (FFI) binding to ROOT. ROOT(http://root.cern.ch) is an object-oriented program and library developed by CERN for physics data analysis."
-                   }
-
-
--}
-{-
-pkg_CORE = mkPkgCfg "HROOT-core" "HROOT.Core" "__HROOT_CORE__" [] (core_classes,core_topfunctions) core_ann "Haskell binding to ROOT Core modules" "HROOT is a haskell Foreign Function Interface (FFI) binding to ROOT. ROOT(http://root.cern.ch) is an object-oriented program and library developed by CERN for physics data analysis."
-pkg_GRAF = mkPkgCfg "HROOT-graf" "HROOT.Graf" "__HROOT_GRAF__" ["HROOT-core","HROOT-hist"] (graf_classes,graf_topfunctions) graf_ann "Haskell binding to ROOT Graf modules" "HROOT is a haskell Foreign Function Interface (FFI) binding to ROOT. ROOT(http://root.cern.ch) is an object-oriented program and library developed by CERN for physics data analysis."
-pkg_HIST = mkPkgCfg "HROOT-hist" "HROOT.Hist" "__HROOT_HIST__" ["HROOT-core"] (hist_classes,hist_topfunctions) hist_ann "Haskell binding to ROOT Hist modules" "HROOT is a haskell Foreign Function Interface (FFI) binding to ROOT. ROOT(http://root.cern.ch) is an object-oriented program and library developed by CERN for physics data analysis."
-pkg_MATH = mkPkgCfg "HROOT-math" "HROOT.Math" "__HROOT_MATH__" ["HROOT-core"] (math_classes,math_topfunctions) M.empty "Haskell binding to ROOT Math modules" "HROOT is a haskell Foreign Function Interface (FFI) binding to ROOT. ROOT(http://root.cern.ch) is an object-oriented program and library developed by CERN for physics data analysis."
-
-
-pkg_IO   = mkPkgCfg "HROOT-io"   "HROOT.IO"   "__HROOT_IO__"   ["HROOT-core"] (io_classes,io_topfunctions) M.empty "Haskell binding to ROOT IO modules" "HROOT is a haskell Foreign Function Interface (FFI) binding to ROOT. ROOT(http://root.cern.ch) is an object-oriented program and library developed by CERN for physics data analysis."
-
-pkg_RooFit = mkPkgCfg "HROOT-RooFit" "HROOT.RooFit" "__HROOT_ROOFIT__" ["HROOT-core", "HROOT-hist", "HROOT-math"] (roofit_classes,roofit_topfunctions) M.empty "Haskell binding to ROOT RooFit modules" "HROOT is a haskell Foreign Function Interface (FFI) binding to ROOT. ROOT(http://root.cern.ch) is an object-oriented program and library developed by CERN for physics data analysis."
-
-
-
-pkg_RooStats =
-    let PkgConfig mods cihs tih _ _ =
-          mkPackageConfig ( "HROOT-RooFit-RooStats"
-                          , mkCROOTIncludeHeaders ([NS "RooStats"],"RooStats"))
-                          (roostats_classes,roostats_topfunctions,[],[])
-        hsbootlst = mkHSBOOTCandidateList mods
-    in PkgCfg { pkgname = "HROOT-RooFit-RooStats"
-              , pkg_summarymodule = "HROOT.RooFit.RooStats"
-              , pkg_typemacro = TypMcro "__HROOT_ROOFIT_ROOSTATS__"
-              , pkg_classes = roostats_classes
-              , pkg_cihs = cihs
-              , pkg_tih = tih
-              , pkg_modules = mods
-              , pkg_annotateMap = M.empty  -- for the time being
-              , pkg_deps = [ "HROOT-core"
-                           , "HROOT-math"
-                           , "HROOT-RooFit"
-                           ]
-              , pkg_hsbootlst = hsbootlst
-              , pkg_synopsis = ""
-              , pkg_description = ""
-              }
-
-
-pkg_TREE = mkPkgCfg "HROOT-tree" "HROOT.Tree" "__HROOT_TREE__" ["HROOT-core"] (tree_classes,tree_topfunctions) tree_ann "Haskell binding to ROOT Tree modules" "HROOT is a haskell Foreign Function Interface (FFI) binding to ROOT. ROOT(http://root.cern.ch) is an object-oriented program and library developed by CERN for physics data analysis."
-
-
-
-
-
--}
