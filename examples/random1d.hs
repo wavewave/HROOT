ace0ff97263197ee9e76c1069ccbeed9482e1900
Ian-Woo Kim <ianwookim@gmail.com>
2019-10-08 22:13:10 -0700

    random1d.hs buildable.

--- examples/random1d.hs
+++ examples/random1d.hs
@@ -1,35 +1,49 @@
 module Main where
 
 import Control.Monad
+import Foreign.C.Types (CDouble)
+import Foreign.C.String (newCString)
+--
+-- import HROOT
+import HROOT.Core
+import HROOT.Hist
+import HROOT.Graf
+import HROOT.Math
 
-import HROOT
 
-main :: IO () 
-main = do 
-  tcanvas <- newTCanvas "Test" "Test" 640 480
-  h1 <- newTH1F "test" "test" 100 (-5.0) 5.0 
+main :: IO ()
+main = do
+  cTest <- newCString "Test"
+  ctest <- newCString "test"
+  cpdf  <- newCString "random1d.pdf"
+  cjpg  <- newCString "random1d.jpg"
+  cpng  <- newCString "random1d.png"
+  cnull <- newCString ""
+
+  tcanvas <- newTCanvas cTest cTest 640 480
+  h1 <- newTH1F ctest ctest 100 (-5.0) 5.0
 
   tRandom <- newTRandom 65535
 
   let generator = gaus tRandom 0 2
 
-  let go n | n < 0 = return () 
-           | otherwise = do 
+  let go n | n < 0 = return ()
+           | otherwise = do
                histfill generator h1
-               go (n-1) 
+               go (n-1)
 
   go 1000000
-  draw h1 ""
-  saveAs tcanvas "random1d.pdf" ""
-  saveAs tcanvas "random1d.jpg" "" 
-  saveAs tcanvas "random1d.png" "" 
+  draw h1 cnull
+  saveAs tcanvas cpdf cnull
+  saveAs tcanvas cjpg cnull
+  saveAs tcanvas cpng cnull
   delete h1
   delete tcanvas
 
 
 
-histfill :: IO Double -> TH1F ->  IO () 
-histfill gen hist = do 
+histfill :: IO CDouble -> TH1F ->  IO ()
+histfill gen hist = do
   x <- gen
   fill1 hist x
-  return () 
+  return ()
