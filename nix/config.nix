f52c359e08224725ce003bad1c821005ca4763ee
Ian-Woo Kim <ianwookim@gmail.com>
2019-10-08 22:00:17 -0700

    static files and examples.

--- nix/config.nix
+++ nix/config.nix
@@ -3,9 +3,10 @@
 let
   fficxxSrc = fetchgit {
     url = "git://github.com/wavewave/fficxx.git";
-    rev = "398a00fc3c2d474b61e729d65e817eefb7e5bfa4";
-    sha256 = "1lylcxnm07njzzfzmg9iq74qpbmdl0vg5pi2mmrakjgf9chxxb3z";
+    rev = "2ddd32a40710558e5ebbd1f3bba0d5951c1dbe8a";
+    sha256 = "15227grpdbpds9dzlnr78l78nx2d1v6kgn0iblnilsmpjk80bxr9";
   };
+
 in
 
 self: super: {
