b8e560dc4d8f9a77e74f303e523a3b13258370c8
Ian-Woo Kim <ianwookim@gmail.com>
2019-10-10 16:37:37 -0700

    gas example.

--- examples/gas.hs
+++ examples/gas.hs
@@ -12,8 +12,10 @@ import Control.Monad         ( forever
                              , when
                              )
 import Control.Monad.Loops   ( iterateM_ )
+import Data.Foldable         ( traverse_ )
 import Data.IORef            ( newIORef, readIORef, modifyIORef' )
 import Data.String           ( IsString(fromString) )
+import Data.Traversable      ( traverse )
 import Foreign.C.Types       ( CDouble, CInt )
 import Foreign.C.String      ( CString, newCString )
 import Foreign.Marshal.Alloc ( alloca )
@@ -23,9 +25,9 @@ import System.IO.Unsafe      ( unsafePerformIO )
 --
 import HROOT
 
+
 instance IsString CString where
   fromString s = unsafePerformIO $ newCString s
-
 
 data Particle =
   Particle {
@@ -33,8 +35,8 @@ data Particle =
   , ptlY :: CDouble
   , ptlPx :: CDouble
   , ptlPy :: CDouble
-  } deriving Show
-
+  , ptlMarker :: TMarker
+  }
 
 generate :: TRandom -> Int -> IO [Particle]
 generate tRandom n = do
@@ -43,19 +45,24 @@ generate tRandom n = do
     y <- gaus tRandom 0 0.5
     dx <- gaus tRandom 0 0.05
     dy <- gaus tRandom 0 0.05
-    pure (Particle x y dx dy)
+    m1 <- newTMarker x y 3
+    draw m1 (""::CString)
 
-step1 :: Particle -> Particle
-step1 (Particle x y dx dy) =
-  Particle (x+dx) (y+dy) dx dy
+    pure (Particle x y dx dy m1)
 
-step :: [Particle] -> [Particle]
-step ps = map step1 ps
+step1 :: Particle -> IO Particle
+step1 (Particle x y dx dy m) = do
+  let x' = x + dx
+      y' = y + dy
+  setX m x
+  setY m y
+  pure $ Particle x' y' dx dy m
 
+step :: [Particle] -> IO [Particle]
+step ps = traverse step1 ps
 
-fillPtls :: TH2F -> [Particle] -> IO ()
-fillPtls hist ps = mapM_ (\(Particle x y _ _) -> void $ fill2 hist x y) ps
-
+release :: Particle -> IO ()
+release (Particle _ _ _ _ m) = delete m
 
 main :: IO ()
 main = do
@@ -66,22 +73,14 @@ main = do
       gsys <- gSystem
       tapp <- newTApplication ("test"::CString) pargc pargv
       tcanvas <- newTCanvas ("Test"::CString) ("Test"::CString) 640 480
-
-      h2 <- newTH2F ("test"::CString) ("test"::CString) 100 (-5.0) 5.0 100 (-5.0) 5.0
-      setMarkerStyle h2 6
-      setMarkerSize h2 5
-
+      range tcanvas (-5.0) (-5.0) 5.0 5.0
       tRandom <- newTRandom 65535
 
       ps₀ <- generate tRandom 100
 
-      draw h2 (""::CString)
-
       forkIO $ flip iterateM_ ps₀ $ \ps -> do
-        threadDelay 1000000
-        reset h2 (""::CString)
-        fillPtls h2 ps
-        let ps' = step ps
+        threadDelay 100000
+        ps' <- step ps
         pure ps'
 
       forkIO $ forever $ do
@@ -93,14 +92,5 @@ main = do
       forever $ do
         threadDelay (1000000 `div` 60) -- every 1/60 sec
         processEvents gsys
-      delete h2
+      traverse_ release ps₀
       delete tapp
-
-
-
-histfill :: IO CDouble -> IO CDouble-> TH2F ->  IO ()
-histfill dist1 dist2 hist = do
-  x <- dist1
-  y <- dist2
-  fill2 hist x y
-  return ()
---
+++ workspace/gas.hs
@@ -0,0 +1,1 @@
+../examples/gas.hs
