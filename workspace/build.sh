67772c2355ea207df15e0ac3644c39a6f0acd4b6
Ian-Woo Kim <ianwookim@gmail.com>
2019-11-02 02:44:35 -0700

    build fix for examples.

--- workspace/build.sh
+++ workspace/build.sh
@@ -2,7 +2,7 @@ rm -rf dist-newstyle
 cd ../HROOT-generate; cabal new-build ; cd ../workspace
 ../HROOT-generate/dist-newstyle/build/x86_64-linux/ghc-8.6.5/HROOT-generate-0.10.0.1/x/HROOT-generate/build/HROOT-generate/HROOT-generate ../HROOT-generate/template
 cabal new-build HROOT
-cabal new-exec -- ghc random2dApp.hs -lstdc++
-cabal new-exec -- ghc graph2d.hs -lstdc++
-cabal new-exec -- ghc httpserver.hs -lstdc++
+cabal new-exec -- ghc random2dApp.hs
+cabal new-exec -- ghc graph2d.hs
+cabal new-exec -- ghc httpserver.hs
 
