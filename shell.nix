821e69301373641ed4093f863e0fa95af0ee322f
Ian-Woo Kim <ianwookim@gmail.com>
2019-11-02 02:49:49 -0700

    back to public source.

--- shell.nix
+++ shell.nix
@@ -3,26 +3,26 @@
 with pkgs;
 
 let
-  #fficxxSrc = fetchgit {
-  #  url = "git://github.com/wavewave/fficxx.git";
-  #  rev = "ac829cd8f315a4a7c1db13db9595f40dda0df7c8";
-  #  sha256 = "1nz3hjx8jqsb200mcg7jm88p1004ka4h11wipzwcay87lxrsa4f1";
-  #};
-  fficxxSrc = /home/wavewave/repo/src/fficxx;
+  fficxxSrc = fetchgit {
+    url = "git://github.com/wavewave/fficxx.git";
+    rev = "21487f389b9f88cad6f506fb0db06534ed96bc28";
+    sha256 = "1b8plcpawd2dv0vy0dzy4dms0afm5dc09yxxgk68x2ghqb1cd0wf";
+  };
 
   newHaskellPackages0 = haskellPackages.override {
     overrides = callPackage ./nix/config.nix { inherit fficxxSrc; };
   };
 
+  stdcxxNix = import ./nix/config-stdcxx.nix {
+    inherit fficxxSrc stdenv fetchgit; packages = newHaskellPackages0;
+  };
 
-  stdcxxNix = import ./nix/config-stdcxx.nix { inherit fficxxSrc stdenv fetchgit; packages = newHaskellPackages0; };
   newHaskellPackages = haskellPackages.override {
     overrides = self: super:
       callPackage ./nix/config.nix { inherit fficxxSrc; } self super //
       { "stdcxx" = self.callPackage stdcxxNix {}; };
 
   };
-
 
   hsenv = newHaskellPackages.ghcWithPackages (p: with p; [
     cabal2nix
