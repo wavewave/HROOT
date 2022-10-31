ed2865dbc6f039d57ee4cb6bf63f02cbeabaeca7
Ian-Woo Kim <ianwookim@gmail.com>
2022-10-31 13:41:02 -0700

    update HROOT as fficxx changed. ROOT build for macosx on aarch64

--- flake.nix
+++ flake.nix
@@ -12,49 +12,52 @@
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = import nixpkgs {
-          overlays = [ fficxx.overlay.${system} ];
           inherit system;
+          config.allowBroken = true;
         };
 
-        finalHaskellOverlay = self: super:
-          (import ./default.nix { inherit pkgs; } self super);
+        root = if system == "aarch64-darwin" then
+          (pkgs.root.override { tbb = null; }).overrideAttrs (old: rec {
+            cmakeFlags = [ "-Dbuiltin_tbb=OFF" ]
+              ++ (builtins.map (x: if (x == "-Dimt=ON") then "-Dimt=OFF" else x)
+                old.cmakeFlags);
+            preBuild = ''
+              export MACOSX_DEPLOYMENT_TARGET="10.16"
+              echo "PRE BUILD HOOK IS CALLED"
+            '';
+          })
+        else
+          pkgs.root;
 
-        newHaskellPackages = pkgs.haskellPackages.extend finalHaskellOverlay;
+        haskellOverlay = final: self: super:
+          (import ./default.nix { pkgs = final; } self super);
 
-      in {
-        packages = {
-          inherit (newHaskellPackages)
+        hpkgsFor = compiler:
+          pkgs.haskell.packages.${compiler}.extend (hself: hsuper:
+            (fficxx.haskellOverlay.${system} pkgs hself hsuper
+              // haskellOverlay pkgs hself hsuper));
+
+        mkPackages = compiler: {
+          inherit (hpkgsFor compiler)
             HROOT HROOT-core HROOT-graf HROOT-hist HROOT-io HROOT-math HROOT-net
             HROOT-tree HROOT-RooFit HROOT-RooFit-RooStats;
         };
 
-        # see these issues and discussions:
-        # - https://github.com/NixOS/nixpkgs/issues/16394
-        # - https://github.com/NixOS/nixpkgs/issues/25887
-        # - https://github.com/NixOS/nixpkgs/issues/26561
-        # - https://discourse.nixos.org/t/nix-haskell-development-2020/6170
-        overlay = final: prev: {
-          haskellPackages = prev.haskellPackages.override (old: {
-            overrides = final.lib.composeExtensions (old.overrides or (_: _: { }))
-              finalHaskellOverlay;
-          });
-        };
-
-        devShells = {
-          "default" = let
-            hsenv = pkgs.haskellPackages.ghcWithPackages
+        mkShellFor = compiler:
+          let
+            hsenv = (hpkgsFor compiler).ghcWithPackages
               (p: [ p.cabal-install p.fficxx p.fficxx-runtime p.stdcxx ]);
           in pkgs.mkShell {
-            buildInputs = [ hsenv pkgs.root ];
+            buildInputs = [ hsenv root pkgs.nixfmt ];
             shellHook = "";
           };
-          "codevelop" = pkgs.haskellPackages.shellFor {
-            packages = ps: [ ps.fficxx ps.fficxx-runtime ];
-            buildInputs = [ pkgs.cabal-install pkgs.root ];
-            withHoogle = false;
-          };
 
-        };
-      }
-  );
+        supportedCompilers = [ "ghc902" "ghc924" "ghc942" ];
+      in {
+        packages =
+          pkgs.lib.genAttrs supportedCompilers (compiler: hpkgsFor compiler);
+
+        devShells =
+          pkgs.lib.genAttrs supportedCompilers (compiler: mkShellFor compiler);
+      });
 }
