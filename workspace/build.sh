3bd2873cf30a5f830114ef1f6d1bcc2a1cc7c1ff
Ian-Woo Kim <ianwookim@gmail.com>
2022-12-30 13:24:46 -0800

    require fficxx >= 0.7

--- workspace/build.sh
+++ workspace/build.sh
@@ -1,4 +1,5 @@
 # delete only generated
+rm -rf working
 rm -rf HROOT
 rm -rf HROOT-core
 rm -rf HROOT-graf
@@ -10,8 +11,8 @@ rm -rf HROOT-RooFit
 rm -rf HROOT-RooFit-RooStats
 rm -rf HROOT-tree
 
+cabal clean
 cabal build HROOT-generate
 cabal run -- HROOT-generate gen -t ../HROOT-generate/template
-#cabal build HROOT-core
 cabal build HROOT
 
